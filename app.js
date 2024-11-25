e9fd32f1acd0984663c72d5b6c182f5413d788bf
rahul-b-p <rahulzantoz10@gmail.com>
2024-11-25 13:07:37 +0530

    routers used

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import { loggers } from './winston/index.js';
+import { ownerRouter, userRouter } from './routers/index.js';
 
 const app = express();
 const port = 3000;
 
-app.listen(port,()=>{
-    loggers.info(`Server Running at http://localhost:${port}`)
+app.use(express.json());
+
+app.use('/admin', ownerRouter);
+app.use('/user', userRouter);
+
+app.listen(port, () => {
+    loggers.info(`Server Running at http://localhost:${port}`);
 })
 
 
